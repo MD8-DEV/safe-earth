dd3a8a757d28db73d2eb1180b6c12d3ff8934ca7
MD8-DEV <mohamedgouda5900@gmail.com>
2022-11-11 18:21:28 +0200

    modified:   safe_earth/__pycache__/settings.cpython-311.pyc 	modified:   safe_earth/__pycache__/urls.cpython-311.pyc 	modified:   safe_earth/urls.py 	modified:   templates/user_auth/dashboard.html 	modified:   templates/user_auth/login.html 	modified:   templates/user_auth/sign_up.html 	modified:   templates/user_auth/style/style.css 	modified:   user_auth/__pycache__/urls.cpython-311.pyc 	modified:   user_auth/__pycache__/views.cpython-311.pyc 	modified:   user_auth/static/style/style.css 	modified:   user_auth/urls.py

--- safe_earth/urls.py
+++ safe_earth/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from user_auth.views import Dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include("user_auth.urls"))
+    path('auth/', include("user_auth.urls")),
+    path('', Dashboard.as_view())
 ]
--- user_auth/urls.py
+++ user_auth/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('login', Login.as_view(), name='login'),
     path('logout', Logout.as_view(), name='logout'),
     path('signup', Signup.as_view(), name='sign'),
-    path('dashboard', Dashboard.as_view(), name="dash")
 ]
