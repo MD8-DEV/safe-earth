f4dead04e3281ccd36a407c4d25b7c199da7023b
MD8 <74047396+MD8-DEV@users.noreply.github.com>
2023-08-27 11:20:43 +0300

    Update settings.py

--- safe_earth/settings.py
+++ safe_earth/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kdb7a1u_t(knf^ezg_f8i2lbmobe-()th#9g8rz+*nil9bqeq@'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
